bdd11fc3ad63d7505f73cb9883fc65be6b2a24fc
TaylorPzreal <taylorpzreal@gmail.com>
2025-03-06 08:15:33 +0800

    chore: update theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -18,6 +18,7 @@ AUTHOR_FEED_RSS = None
 # Blogroll
 LINKS = (
     ("Github", "https://github.com/TaylorPzreal"),
+    ("Pelican", "https://getpelican.com/")
 )
 
 # Social widget
@@ -25,7 +26,11 @@ LINKS = (
 #     ("You can add links in your config file", "#"),
 # )
 
-DEFAULT_PAGINATION = 10
+DEFAULT_PAGINATION = 5
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
+
+# COPYRIGHT_YEAR=2025
+# TRADEMARK = '你的商标'
+# FOOTER_CONTENT = f'© {COPYRIGHT_YEAR} {AUTHOR} | {TRADEMARK} 保留所有权利。'
